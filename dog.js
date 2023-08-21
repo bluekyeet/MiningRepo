0aea2635cba2abea67f2fe532513f2c445e8a441
bluekyeet <56775066+bluekyeet@users.noreply.github.com>
2023-08-21 19:53:09 +1200

    Update dog.js

--- dog.js
+++ dog.js
@@ -1,7 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  if (typeof adBlockDetector === "undefined") {
-    setTimeout(function() {
-      location.reload();
-    }, 5000);
-  }
-});
+		if(adblock) {
+			location.reload();
+		}
