2c282173cbdd8f1721b19ccff4e01f6b1c586ebc
bluekyeet <56775066+bluekyeet@users.noreply.github.com>
2023-08-21 19:54:44 +1200

    Update dog.js

--- dog.js
+++ dog.js
@@ -1,3 +1,26 @@
-		if(adblock) {
-			location.reload();
-		}
+<script src=
+"https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js">
+</script>
+<script>
+	(function() {
+	var detector = function() {
+		setTimeout(function() {
+			if(!document.getElementsByClassName) return;
+			var ads =
+			document.getElementsByClassName('banner_ad'),
+			ad = ads[ads.length - 1];
+
+			if(!ad || ad.innerHTML.length == 0 
+			|| ad.clientHeight === 0) {
+				console.log('Ad Blocker Detected');
+			} else {
+				console.log('No Ad Blocker');
+			}
+		}, 2000);
+	}
+	/* Add a page load listener */
+	if(window.addEventListener) {
+		window.addEventListener('load', detector, false);
+	}
+	})();
+</script>
